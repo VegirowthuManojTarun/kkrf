b8bf895256bd280771707bcb5cd8bb4467a1bd34
VegirowthuManojTarun <manojtarun934.com>
2025-01-01 15:42:53 +0530

    carousel implemented

---
+++ kkrf-proj1-main/script.js
@@ -0,0 +1,9 @@
+// let currentIndex = 0;
+// const slides = document.querySelectorAll('.carousel-item');
+// const totalSlides = slides.length;
+
+// // Automatically switch slides at intervals
+// setInterval(() => {
+//   currentIndex = (currentIndex + 1) % totalSlides;
+//   document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
+// }, 3000);
